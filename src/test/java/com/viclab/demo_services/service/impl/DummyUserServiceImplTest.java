aac6156bd707ed13b2a7be3f769ad08273296bb0
wzack9678 <wzack9678@gmail.com>
2025-09-27 00:17:08 -0400

    add unit tests for DummyUserServiceImpl

---
+++ src/test/java/com/viclab/demo_services/service/impl/DummyUserServiceImplTest.java
@@ -0,0 +1,34 @@
+package com.viclab.demo_services.service.impl;
+
+import com.viclab.demo_services.payload.UserDTO;
+import com.viclab.demo_services.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class DummyUserServiceImplTest {
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        userService = new DummyUserServiceImpl();
+    }
+
+    @Test
+    @DisplayName("Should create a new user with correct ID and status")
+    void createUser_shouldAssignIncrementalId_andReturnSavedUser () {
+        UserDTO req = new UserDTO();
+        req.setName("Alice");
+        req.setEmail("alice@test.com");
+        req.setPhone("123456");
+        req.setStatus("ACTIVE");
+
+        UserDTO saved = userService.createUser(req);
+        assertNotNull(saved.getId());
+        assertEquals("Alice", saved.getName());
+        assertEquals("ACTIVE", saved.getStatus());
+    }
+}
